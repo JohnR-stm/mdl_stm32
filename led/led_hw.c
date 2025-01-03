50e893d1b66b2603b1cc9f3a6de6f9c3d5781e53
Ivan Rom <u235rv@gmail.com>
2025-01-03 13:13:34 +0300

    fixed module led_hw

--- led/led_hw.c
+++ led/led_hw.c
@@ -2,17 +2,13 @@
 
 #ifdef STM32G0
 #include "stm32g0xx_ll_bus.h"
-#include "stm32g0xx_ll_rcc.h"
 #include "stm32g0xx_ll_gpio.h"
-#include "stm32g0xx_ll_usart.h"
 #endif /* STM32G0 */
 
-#ifdef STM32L4
-#include "stm32l4xx_ll_bus.h"
-#include "stm32l4xx_ll_rcc.h"
-#include "stm32l4xx_ll_gpio.h"
-#include "stm32l4xx_ll_usart.h"
-#endif /* STM32L4 */
+#ifdef STM32F3
+#include "stm32f3xx_ll_bus.h"
+#include "stm32f3xx_ll_gpio.h"
+#endif /* STM32F3 */
 
 #include "led_config.h"
 
@@ -24,23 +20,18 @@
 
 void led_init(void)
 {
+  //--- GPIO Ports Clock Enable ---//
+  if  ( LED_CLK_IsEnabled(LED_GREEN_BUS) )
+  { } else LED_CLK_Enable(LED_GREEN_BUS);
+  //--- GREEN LED Init ---//
   LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-  /* GPIO Ports Clock Enable */
-  LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOC);
-  LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOF);
-  LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOA);
-
-  /**/
-  LL_GPIO_ResetOutputPin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
-
-  /**/
-  GPIO_InitStruct.Pin = LED_GREEN_Pin;
+  LL_GPIO_ResetOutputPin(LED_GREEN_PORT, LED_GREEN);
+  GPIO_InitStruct.Pin = LED_GREEN;
   GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
   GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
   GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
   GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-  LL_GPIO_Init(LED_GREEN_GPIO_Port, &GPIO_InitStruct);
+  LL_GPIO_Init(LED_GREEN_PORT, &GPIO_InitStruct);
 }
 
 //-----------------------------------------------------------------------------
@@ -49,7 +40,7 @@ void led_init(void)
 
 void led_green_on(void)
 {
-  LL_GPIO_SetOutputPin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
+  LL_GPIO_SetOutputPin(LED_GREEN_PORT, LED_GREEN);
 }
 
 //-----------------------------------------------------------------------------
@@ -58,7 +49,7 @@ void led_green_on(void)
 
 void led_green_off(void)
 {
-  LL_GPIO_ResetOutputPin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
+  LL_GPIO_ResetOutputPin(LED_GREEN_PORT, LED_GREEN);
 }
 
 //-----------------------------------------------------------------------------
